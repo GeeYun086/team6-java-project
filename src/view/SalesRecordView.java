e440bc50e4d88fd974cf58a98d5694d3cc8d1eb2
ha2515 <yoojin2515@gmail.com>
2026-06-05 22:20:14 +0900

    관리자 재고 수정

--- src/view/SalesRecordView.java
+++ src/view/SalesRecordView.java
@@ -21,19 +21,24 @@ public class SalesRecordView extends JFrame {
             List<SalesRecord> records) {
 
         setTitle("판매 기록 조회");
-        setSize(500, 400);
+        setSize(650, 450);
+        setLocationRelativeTo(null);
 
+        Color bgColor = new Color(24, 26, 40);
+        Color panelColor = new Color(32, 35, 55);
+        Color textColor = new Color(225, 228, 240);
+
+        getContentPane().setBackground(bgColor);
         recordArea = new JTextArea();
         recordArea.setEditable(false);
+        recordArea.setFont(new Font("맑은 고딕",Font.PLAIN, 14));
+        recordArea.setBackground(panelColor);
+        recordArea.setForeground(textColor);
 
         displayRecords(records);
-
-        add(
-                new JScrollPane(recordArea),
-                BorderLayout.CENTER
-        );
-
-        setLocationRelativeTo(null);
+        JScrollPane scrollPane = new JScrollPane(recordArea);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        add(scrollPane, BorderLayout.CENTER);
     }
 
     /** 판매 기록 출력 */
